969a00decad6ffcbf884b657902f194411010859
Luka Lazic <luka.web.php@gmail.com>
2022-12-04 18:18:38 +0100

    more fine tuning

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
     extend: {
       colors: {
         primary: "#121549",
-        backgroundColor: "#121549",
+        backgroundColor: "#131546",
       },
       fontSize: {
         xsm: "0.9rem",
+        xxsm: "0.75rem",
       },
     },
   },
